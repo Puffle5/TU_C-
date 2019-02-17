3cc02d42c3ad10a488fb778df85e78196a73ec09
Puffle5 <elena_haruno@abv.bg>
2019-02-17 17:38:22 +0200

    Passing methods to functions & Ref/Out.

--- UniversitySystem/Concepts/Program.cs
+++ UniversitySystem/Concepts/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Concepts.MethodParameters;
 
 namespace Concepts
 {
@@ -11,13 +12,39 @@ class Program
 static void Main(string[] args)
 {
 Console.WriteLine("Hello!");
-//StaticMembers.Reset();
-Console.WriteLine(StaticMembers.Name);
+Animal cat = new Animal();
+Animal dog = new Animal();
+cat.weight = 30;
+dog.weight = 60;
+changePrimitiveWeight(cat.weight);
+changeReferenceWeight(dog);
+Console.WriteLine("Cat: "+cat.weight+", Dog: "+dog.weight);
 
+changeReferenceWithRef(ref dog);
+Console.WriteLine(dog.weight);
 
-Properties property = new Properties();
-property.IQ = 5;
-Console.WriteLine(property.IQ);
+int number = 8;
+changePrimitiveWithRef(ref number);
+Console.WriteLine(number);
 
 Console.ReadLine();
-}}}
+}
+    //This method will not change the current weight outside its scope.
+static void changePrimitiveWeight(double currentWeight)
+{ currentWeight = 8; }
+static void changeReferenceWeight(Animal animalObject) 
+{
+    Animal penguin = new Animal();
+    penguin.weight = 20;
+    animalObject = penguin;
+}
+
+static void changeReferenceWithRef(ref Animal animalObject) 
+{
+Animal frog = new Animal();
+frog.weight = 2;
+animalObject = frog;
+}
+static void changePrimitiveWithRef(ref int number) 
+{number = 54;}
+}}
