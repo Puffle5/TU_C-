d233679b9002ed8f9499d99a7d1ee9c694ac568f
Puffle5 <elena_haruno@abv.bg>
2019-02-23 22:41:08 +0200

    Switch-Case (welcome message) for user roles.

--- UniversitySystem/UniversitySystem/Program.cs
+++ UniversitySystem/UniversitySystem/Program.cs
@@ -19,7 +19,18 @@ LoginValidation validation = new LoginValidation(userName, password);
 User user = new User();
 
 if (validation.ValidateUserInput(user))
-{ Console.WriteLine(user.Username + ", " + user.Password + ", " + user.FakNum + ", " + user.Role); }
+{ 
+Console.WriteLine(user.Username + ", " + user.Password + ", " + user.FakNum + ", " + user.Role);
+switch (user.Role) 
+{
+    case 0: Console.WriteLine("Welcome, ANONYMOUS!"); break;
+    case 1: Console.WriteLine("Welcome, ADMIN!"); break;
+    case 2: Console.WriteLine("Welcome, INSPECTOR!"); break;
+    case 3: Console.WriteLine("Welcome, PROFESSOR!"); break;
+    case 4: Console.WriteLine("Welcome, STUDENT!"); break;
+    
+}
+}
 
 else 
 {
