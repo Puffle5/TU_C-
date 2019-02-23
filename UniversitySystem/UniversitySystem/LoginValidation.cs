5d6b662f6e4e27216929cc8dba6af060dfa980dc
Puffle5 <elena_haruno@abv.bg>
2019-02-23 22:01:52 +0200

    IsUserPassCorrect(username, password) in ValidateUserInput(User user).

--- UniversitySystem/UniversitySystem/LoginValidation.cs
+++ UniversitySystem/UniversitySystem/LoginValidation.cs
@@ -6,13 +6,10 @@ using System.Threading.Tasks;
 
 namespace UniversitySystem
 {
-    class LoginValidation
-    {
-        public static UserRoles CurrentUserRole
-        {
-            get { return currentUserRole; }
-            private set { currentUserRole = value; }
-        }
+class LoginValidation
+{
+public static UserRoles CurrentUserRole
+{get {return currentUserRole;} private set {currentUserRole = value;}}
         private static UserRoles currentUserRole;
         private String userName;
         private String password;
@@ -27,13 +24,21 @@ namespace UniversitySystem
 
             Boolean emptyUserName = this.userName.Equals(String.Empty); 
             Boolean emptyPassWord = this.password.Equals(String.Empty);
-            addedUser.Username = this.userName;
-            addedUser.Password = this.password;
+           
             currentUserRole = (UserRoles)addedUser.Role;
+
+
             if (emptyUserName) {this.errorMessage = "Missing username!"; return false;}
             if (emptyPassWord) {this.errorMessage = "Missing password!"; return false;}
-            if (this.userName.Length < 5) { this.errorMessage = "Username too short!"; return false; }
-            if (this.password.Length < 5) { this.errorMessage = "Password too short!"; return false; }
+            if (this.userName.Length < 5) {this.errorMessage = "Username too short!"; return false;}
+            if (this.password.Length < 5) {this.errorMessage = "Password too short!"; return false;}
+            User findUser = UserData.IsUserPassCorrect(this.userName, this.password);
+            if (findUser == null) {return false;}
+            addedUser.Username = findUser.Username;
+            addedUser.Password = findUser.Password;
+            addedUser.FakNum = findUser.FakNum;
+            addedUser.Role = findUser.Role;
+
             return true;
         
         }
