5f4c1c16ab8a352d29e84f101e04f27e5bef4ce7
Puffle5 <elena_haruno@abv.bg>
2019-02-23 19:58:11 +0200

    Added input capabilities for log-in.

--- UniversitySystem/UniversitySystem/Program.cs
+++ UniversitySystem/UniversitySystem/Program.cs
@@ -11,8 +11,13 @@ class Program
  static void Main(string[] args)
 {
 User Admin = UserData.TestUser;
-     
-LoginValidation validation = new LoginValidation();
+Console.WriteLine("Enter username:");
+String userName = Console.ReadLine();
+Console.WriteLine("Enter password:");
+String password = Console.ReadLine();
+LoginValidation validation = new LoginValidation(userName, password);
+
+
 if (validation.ValidateUserInput(Admin)) 
 {Console.WriteLine(Admin.Username+", "+Admin.Password+", "+Admin.FakNum+", "+Admin.Role);}
 Console.WriteLine(LoginValidation.CurrentUserRole);
