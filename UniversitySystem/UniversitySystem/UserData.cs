59bef8621d1c66afdc212cf9ee4843cd4cdfbdc1
Puffle5 <elena_haruno@abv.bg>
2019-02-23 21:42:41 +0200

    Adding IsUserPassCorrect(username, password).

--- UniversitySystem/UniversitySystem/UserData.cs
+++ UniversitySystem/UniversitySystem/UserData.cs
@@ -19,8 +19,20 @@ set {testUsers = value;}
 private static void ResetUserData()
 {
 testUsers = new User[3];
-testUsers[0] = new User("Puffle5", "12345", "121216007", 0);
-testUsers[1] = new User("John", "12321", "121216678", 0);
-testUsers[2] = new User("Petur40", "1234567890", "121216123", 0);
+testUsers[0] = new User("Puffle5", "12345", "121216007", 1);
+testUsers[1] = new User("Johnson", "12321", "121216678", 4);
+testUsers[2] = new User("Petur40", "1234567890", "121216123", 4);
 //testUsers = new User[] {--"--};
-}}}
+}
+public static User IsUserPassCorrect(String username, String password)
+{
+    for (int i = 0; i < testUsers.Length; i++) 
+    {
+        if (testUsers[i].Username == username && testUsers[i].Password == password)
+        {
+            return testUsers[i];
+        }
+
+    } return null;
+}
+}}
