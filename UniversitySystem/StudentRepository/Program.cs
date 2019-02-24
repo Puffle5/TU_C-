b12009beb5ebf11af2d8ffe6d7545b46a21223ae
Puffle5 <elena_haruno@abv.bg>
2019-02-24 22:09:56 +0200

    String --> Int --> DateTime.

--- UniversitySystem/StudentRepository/Program.cs
+++ UniversitySystem/StudentRepository/Program.cs
@@ -18,6 +18,14 @@ static void Main(string[] args)
     Console.WriteLine("Today: "+DateTime.Today);
     //Date in 12 Hours:
     Console.WriteLine("Date in 12 Hours: "+DateTime.Now.AddHours(12));
+    Console.WriteLine("Enter Year: ");
+    int year = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter Month: ");
+    int month = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter Day: ");
+    int day = int.Parse(Console.ReadLine());
+    DateTime Date = new DateTime(year, month, day);
+    Console.WriteLine(Date);
 
 
     Console.ReadLine();
