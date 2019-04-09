7b318437039941c552e29f52161fb741a6e638a2
Puffle5 <elena_haruno@abv.bg>
2019-04-09 16:30:34 +0300

    Stage 1.

--- UniversitySystem/StudentInfoSystem/MainWindow.xaml.cs
+++ UniversitySystem/StudentInfoSystem/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace StudentInfoSystem
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Page
     {
         public MainWindow()
         {
